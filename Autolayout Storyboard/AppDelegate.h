3e52d4e1b3f5680fd9c0379181383c9d9cb5df60
Chelsea Liu <cl314@duke.edu>
2015-06-17 12:38:03 -0400

    Initial Commit

---
+++ Autolayout Storyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Autolayout Storyboard
+//
+//  Created by Chelsea Liu on 6/17/15.
+//  Copyright (c) 2015 Chelsea Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
